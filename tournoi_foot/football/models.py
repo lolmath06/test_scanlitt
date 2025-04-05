f4ffffb256877e5156066b9b38d9f31cfe530688
lolmath06 <matheo.dolmen@epitech.eu>
2025-04-05 03:48:12 +0200

    partie 3

--- tournoi_foot/football/models.py
+++ tournoi_foot/football/models.py
@@ -1,3 +1,4 @@
+# football/models.py
 from django.db import models
 
 class Equipe(models.Model):
@@ -17,6 +18,11 @@ class Joueur(models.Model):
 
     nom = models.CharField(max_length=100)
     poste = models.CharField(max_length=20, choices=POSTES)
+    equipe = models.ForeignKey(Equipe, related_name='joueurs', on_delete=models.CASCADE)  # <-- ajout ici
 
     def __str__(self):
         return f"{self.nom} ({self.poste})"
+
+    def clean(self):
+        if self.equipe.joueurs.count() >= 11:
+            raise ValidationError("Cette équipe a déjà atteint la limite maximale de 11 joueurs.")

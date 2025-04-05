20fe98f0fa5681e1d604e3159439eb0b00de2983
lolmath06 <matheo.dolmen@epitech.eu>
2025-04-05 17:26:00 +0200

    partie 4

--- tournoi_foot/football/tests.py
+++ tournoi_foot/football/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Equipe, Joueur
 
-# Create your tests here.
+class EquipeJoueurTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.equipe = Equipe.objects.create(nom="OGC Nice", ville="Nice")
+
+    def test_creation_equipe(self):
+        response = self.client.post('/api/equipes/', {'nom': 'PSG', 'ville': 'Paris'})
+        self.assertEqual(response.status_code, 201)
+
+    def test_limite_joueurs_equipe(self):
+        for i in range(11):
+            Joueur.objects.create(nom=f'Joueur{i}', poste='Gardien', equipe=self.equipe)
+        response = self.client.post('/api/joueurs/', {'nom': 'ExtraJoueur', 'poste': 'Gardien', 'equipe': self.equipe.id})
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Cette équipe a déjà 11 joueurs.', str(response.data))
